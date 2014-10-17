c6d1543bba18197b5a7dd6b40b529fe96542c52f
asanrivas <asanrivas@gmail.com>
2014-10-17 11:34:16 +0800

    Change package name

--- src/android/FileOpener.java
+++ src/android/FileOpener.java
@@ -6,7 +6,7 @@
  * Copyright (c) 2011, IBM Corporation
  */
 
-package com.phonegap.plugins.fileopener;
+package com.phonegap.cordova;
 
 import java.io.IOException;
 import org.apache.cordova.CallbackContext;
