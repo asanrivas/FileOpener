1eaf1aea6a66861480c1bc154b8fd3db93844de7
asanrivas <asanrivas@gmail.com>
2014-10-17 10:21:42 +0800

    Update

--- src/android/FileOpener.java
+++ src/android/FileOpener.java
@@ -8,21 +8,17 @@
 
 package com.phonegap.plugins.fileopener;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-
-import android.content.Context;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 
 
 public class FileOpener extends CordovaPlugin {
@@ -31,12 +27,16 @@ public class FileOpener extends CordovaPlugin {
     public boolean execute(String action, JSONArray args,
                     CallbackContext callbackContext) throws JSONException {
 
-                PluginResult.Status status = PluginResult.Status.OK;
+            PluginResult.Status status = PluginResult.Status.OK;
             String result = "";
 
             try {
                 if (action.equals("openFile")) {
-                    openFile(args.getString(0));
+                	if(args.getString(1)==null||args.getString(1)=="null")
+                		
+                		openFile(args.getString(0));
+                	else
+                		openFile(args.getString(0), args.getString(1));
                 }
                 else {
                     status = PluginResult.Status.INVALID_ACTION;
@@ -115,11 +115,102 @@ public class FileOpener extends CordovaPlugin {
         //so you can choose which application to use
 
 
-        else {            intent = new Intent(Intent.ACTION_VIEW);
+        else {            
+        	intent = new Intent(Intent.ACTION_VIEW);
             intent.setDataAndType(uri, "*/*");
         }
 
         this.cordova.getActivity().startActivity(intent);
+    }
+    private void openFile(String url, String type) throws IOException {
+        // Create URI
+        Uri uri = Uri.parse(url);
+
+        Intent intent = null;
+        Log.v("FileOpener", "Type: "+type);
+        
+        if(type.equals("pdfshare"))
+        {
+        	 intent = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+            // intent.setDataAndType(uri, "application/pdf");
+             intent.setType("application/pdf");
+             intent.putExtra(Intent.EXTRA_SUBJECT, "AMR Report");
+             intent.putExtra(Intent.EXTRA_TEXT, "");
+             intent.putExtra(Intent.EXTRA_STREAM,uri);
+             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        } else if(url.contains(".pdf")) {
+            // PDF file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/pdf");
+        } else if(url.contains(".ppt") || url.contains(".pptx")) {
+            // Powerpoint file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+        } else if(url.contains(".xls") || url.contains(".xlsx")) {
+            // Excel file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/vnd.ms-excel");
+        } else if(url.contains(".rtf")) {
+            // RTF file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/rtf");
+        } else if(url.contains(".wav")) {
+            // WAV audio file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "audio/x-wav");
+        } else if(url.contains(".gif")) {
+            // GIF file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "image/gif");
+        } else if(url.contains(".jpg") || url.contains(".jpeg")) {
+            // JPG file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "image/jpeg");
+        } else if(url.contains(".png")) {
+            // PNG file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "image/png");
+        } else if(url.contains(".txt")) {
+            // Text file
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "text/plain");
+        } else if(url.contains(".mpg") || url.contains(".mpeg") || url.contains(".mpe") || url.contains(".mp4") || url.contains(".avi")) {
+            // Video files
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "video/*");
+        } else if (url.contains(".doc") || url.contains(".docx")) {
+            // Word document
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "application/msword");
+        }  
+
+        //if you want you can also define the intent type for any other file
+
+        //additionally use else clause below, to manage other unknown extensions
+        //in this case, Android will show all applications installed on the device
+        //so you can choose which application to use
+
+        
+        else if (type.equals("none")||type.equals("*/*")){            
+        	intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "*/*");
+        }
+        else
+        {
+        	intent = new Intent(Intent.ACTION_VIEW);
+        	intent.setAction(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, type);
+        }
+        
+        //TRY Catch error
+        try 
+        {
+        	this.cordova.getActivity().startActivity(intent);
+        }
+        catch(ActivityNotFoundException e) {
+        	intent.setData(uri);
+        	this.cordova.getActivity().startActivity(intent);
+        }
     }
 
 }
